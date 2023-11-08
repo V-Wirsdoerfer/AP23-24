edc26d9598395926179c65fbaf4fabc9437da263
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2023-11-08 12:47:05 +0100

    versuch des run mit biber lit.bib in v204 hinzugefügt

---
+++ v204/plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 1000)
+y = x ** np.cos(x)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
+ax1.plot(x, y, label="Vincent test Kurve")
+ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+ax1.legend(loc="best")
+
+ax2.plot(x, y, label="Joris test Kurve")
+ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+ax2.legend(loc="best")
+
+fig.savefig("build/plot.pdf")
