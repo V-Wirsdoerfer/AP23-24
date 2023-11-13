1fb713a17c7795a0f352bd2f7f2d21333e8ecf4d
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2023-11-13 16:17:31 +0100

    2nd work on Hook

--- Uebung_Hook/plot.py
+++ Uebung_Hook/plot.py
@@ -2,9 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x, F = np.genfromtxt("content/Daten.txt", unpack = True)
+x_dots = np.linspace(0, 0.6, 1000)
+
+
+#lineare regression
+params, covariance_matrix = np.polyfit(x, F, deg=1, cov=True)
+
+errors =np.sqrt(np.diag(covariance_matrix))
 
 fig, ax = plt.subplots()
 
-ax.plot(x,F)
+#Messwerte plotten
+ax.plot(x,F, "x", label="Messdaten")
 
-plt.show()
+#Regressionsgerade plotten
+ax.plot(x_dots, params[0]*x_dots + params[1], label = "lineare Regression")
+
+#Plot schön machen
+ax.set(
+    xlabel = r"$\text{Auslenkung} \; \increment x$",
+    ylabel = r"$\text{Kraft} \; F$",
+)
+ax.legend()
+
+
+fig.savefig("build/plot.pdf")
