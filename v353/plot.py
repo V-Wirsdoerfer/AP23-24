1f87077aa0fea7384703593608c9068fc5f1a416
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2023-11-23 13:19:16 +0100

    plot ist kacke

--- v353/plot.py
+++ v353/plot.py
@@ -9,8 +9,9 @@ from scipy.optimize import curve_fit
 
 # Daten generieren
 t, U = np.genfromtxt("./content/RC-daten.txt", unpack=True)
-f, A, phi = np.genfromtxt("content/f-A-phi.txt", unpack=True)
-
+f, A, a = np.genfromtxt("content/f-A-a.txt", unpack=True)
+b = 1 / f
+phi = a / b * 2 * np.pi
 
 # Rohdaten plotten
 fig1, ax1 = plt.subplots()
@@ -42,7 +43,7 @@ omega = f * 2 * np.pi
 
 
 def A_w(omega, a, b):
-    return (b / np.sqrt(1 + omega**2 * a**2))
+    return b / np.sqrt(1 + omega**2 * a**2)
 
 
 fig2, ax2 = plt.subplots()
@@ -51,39 +52,48 @@ ax2.set(
     xlabel=r"$v_i/s^{-1}$",
     ylabel=r"$A(v_i)/mV$",
     yscale="log",
-    ylim = (0,1),
+    ylim=(0, 1),
 )
 
 a = -15
 b = 5
 b = 40000000
-x = np.linspace(10, 650000,1000)
+x = np.linspace(10, 650000, 1000)
 params, covariance_matrix = curve_fit(A_w, omega, A, p0=(a, b))
 ax2.plot(x, A_w(x, *params), label="curve fit")
-ax2.plot(x, (b / np.sqrt(1 + x**2 * a**2)),label="per Hand")
+ax2.plot(x, (b / np.sqrt(1 + x**2 * a**2)), label="per Hand")
 
 ax2.legend()
 print(params)
 
 fig3, ax3 = plt.subplots()
-ax3.plot(omega, phi, "x", label = "Phasenverschiebung")
+ax3.plot(omega, phi, "x", label="Phasenverschiebung")
 ax3.legend()
 ax3.set(
     xlabel=r"$\nu_i$",
     ylabel=r"$\phi(\nu_i)$",
-    yscale = "log",
+    yscale="log",
 )
 
-x = 2* np.pi * np.linspace(30,100000)
-a = 0.00051
-b=1e-5
+x = 2 * np.pi * np.linspace(30, 100000,)
+a = -0.0000000052
+# b=5e-0
+# c=-1e4
 
-def phi_w(omega, a,b):
-    return np.cos(- omega * a)/np.sin(- omega * a) +b
+a, b, c = 1, 1, 1
 
-params, covariance_matrix = curve_fit(phi_w, omega, phi, p0 = (a,b))
-ax3.plot(x, np.cos(- x * a)/np.sin(- x * a) +b, label = "per hand" )
+
+def phi_w(omega, a, b, c):
+    return np.arctan(-omega * a) * b + c
+
+
+params, covariance_matrix = curve_fit(phi_w, omega, a, p0=(a, b, c))
+ax3.plot(
+    x,
+    np.cos(-x * params[0]) / np.sin(-x * params[0]) * params[1] + params[2],
+    label="per hand",
+)
+#ax3.plot(x, np.cos(-x * a) / np.sin(-x * a) * b + c, label="per hand")
 print("params ax3: ", *params)
 ax3.legend()
 plt.show()
-
