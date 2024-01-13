df6c950d7306a0d42375845a984527a0cc099ed9
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-01-13 23:09:12 +0100

    weitere Ergänzungen, noch nicht kontrolliert!

--- v207/plot.py
+++ v207/plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 from uncertainties import ufloat
+from scipy import stats
 from scipy.stats import sem
 import uncertainties.unumpy as unp
 
@@ -26,6 +27,7 @@ K_kl = 0.07640e-6 # in Pa m3/kg
 rho_kl = (0.0049528) / (4 / 3 * np.pi * (D_gr / 2) ** 3) # in kg/m^3
 rho_gr = 0.0044531 / (4 / 3 * np.pi * (0.5 * D_kl) ** 3) # in kg/m^3 
 rho_Fl = 0.99821e3 #kg/m^3
+V_kl = 4/3 * np.pi * (D_kl/ 2) ** 3 #in m^3
 V_gr = 4/3 * np.pi * (D_gr/ 2) ** 3 #in m^3
 
 print("Dichte große Kugel = ", rho_gr)
@@ -43,6 +45,19 @@ def get_K_gr(t):
         (rho_gr.nominal_value - rho_Fl) * t
     )  # evtl noch t -> 2t, da nur die Hälfte der Zeit
 
+def get_Re_kl(t):
+    return rho_Fl * V_kl * 10 / get_eta_kl(t) # in m hoffentlich
+
+def Re_gr(t):
+    return rho_Fl * V_gr * 5 / get_eta_gr(t) # in m hoffentlich
+
+def Re_d(eta):
+    #print("rho_Fl: ", rho_Fl, "\n V_gr: ", V_gr, "\neta: ", eta)
+    return rho_Fl * V_gr * 5 / eta # in m hoffentlich
+
+
+
+
 
 ######################################
 # Die Auswertung erfolgt der Reihenfolge nach. Erst werden die Daten der kleinen Kugel gesichtet,
@@ -55,7 +70,7 @@ def get_K_gr(t):
 fig1, ax1 = plt.subplots(
     layout="constrained", label="Kontrollplot kleine Kugel 20 grad"
 )
-ax1.plot(tk_ou, get_eta_kl(tk_ou) * 1000, "rx", label="oben nach unten")
+ax1.plot(tk_ou, get_eta_kl(tk_ou) * 1000, "rx", label="oben nach unten") # mal 1000 damit von si einheiten auf die abgebildeten
 ax1.plot(tk_uo, get_eta_kl(tk_uo) * 1000, "gx", label="unten nach oben")
 
 ax1.set(
@@ -65,10 +80,15 @@ ax1.set(
 ax1.legend()
 fig1.savefig("build/klein_20C.pdf")
 
-# Viskositäten berechnen und dann über alle Mitteln
+# Viskositäten berechnen und dann über alle Mitteln ### kleine Kugel in kg m^-1 s^-1
 ar_eta_kl = np.concatenate((np.array(get_eta_kl(tk_ou)), np.array(get_eta_kl(tk_uo))))
 eta_kl = ufloat(np.mean(ar_eta_kl), scipy.stats.sem(ar_eta_kl))
-print("Das ist die Viskosität über die kleine Kugel berechnet: ", eta_kl)
+print("Viskosität kleine Kugel: ", eta_kl)
+
+# Reynoldszahlen berechnen und dann über alle Mitteln ### kleine Kugel
+ar_Re_kl = np.concatenate((np.array(get_Re_kl(tk_ou)), np.array(get_Re_kl(tk_uo))))
+Re_kl = np.mean(ar_Re_kl)
+print("Reynoldszahl kleine Kugel: ", Re_kl)
 
 
 # Apparaturkonstante aus den einzelnen Viskositäten berechnen
@@ -76,14 +96,14 @@ ar_K_gr = np.concatenate(
     (get_K_gr(tg_ou1), get_K_gr(tg_ou2), get_K_gr(tg_uo1), get_K_gr(tg_uo2))
 )
 K_gr = ufloat(np.mean(ar_K_gr), scipy.stats.sem(ar_K_gr))
-print("Das ist die Apparaturkonstante der großen Kugel: ", K_gr)
+print("Apparaturkonstante der großen Kugel: ", K_gr)
 
 # Kontrollplot 20 grad große Kugel
 fig2, ax2 = plt.subplots(label="Kontrollplot große Kugel 20 grad")
-ax2.plot(tg_ou1, get_K_gr(tg_ou1), "rx", label="1 oben nach unten")
-ax2.plot(tg_ou2, get_K_gr(tg_ou2), "gx", label="2 oben nach unten")
-ax2.plot(tg_uo1, get_K_gr(tg_uo1), "bx", label="1 unten nach oben")
-ax2.plot(tg_uo2, get_K_gr(tg_uo2), "kx", label="2 unten nach oben")
+ax2.plot(tg_ou1, get_K_gr(tg_ou1) * 1e6, "rx", label="1 oben nach unten") # durch 1e-6 damit von SI auf andere Einheiten 
+ax2.plot(tg_ou2, get_K_gr(tg_ou2) * 1e6, "gx", label="2 oben nach unten") # durch 1e-6 damit von SI auf andere Einheiten 
+ax2.plot(tg_uo1, get_K_gr(tg_uo1) * 1e6, "bx", label="1 unten nach oben") # durch 1e-6 damit von SI auf andere Einheiten 
+ax2.plot(tg_uo2, get_K_gr(tg_uo2) * 1e6, "kx", label="2 unten nach oben") # durch 1e-6 damit von SI auf andere Einheiten 
 
 ax2.set(
     xlabel=r"$t / \unit{\second}$",
@@ -139,12 +159,23 @@ B = ufloat(params[0], err[0])
 print("Debug: ", err)
 print("Koeffizient A = ", A, "\nKoeffizient B = ", B)
 
-#Reynoldazahl
+# Viskositäten berechnen und dann über alle Mitteln ### große Kugel
+ar_eta_gr = np.concatenate((np.array(get_eta_gr(tg_ou1)), np.array(get_eta_gr(tg_uo2)), np.array(get_eta_gr(tg_ou1)), np.array(get_eta_gr(tg_uo2))))
+ar_eta_gr *= 1000
+eta_gr = np.mean(ar_eta_gr)
+print("Viskositätgroße Kugel: ", eta_gr)
 
-def Re(eta):
-    print("rho_Fl: ", rho_Fl, "\n V_gr: ", V_gr, "\neta: ", eta)
-    return rho_Fl * V_gr * 5 / eta # in m hoffentlich
+# Reynoldszahlen berechnen und dann über alle Mitteln ### große Kugel
+ar_Re_gr = np.concatenate((np.array(Re_gr(tg_ou1)), np.array(Re_gr(tg_uo2)), np.array(Re_gr(tg_ou1)), np.array(Re_gr(tg_uo2))))
+num_Re_gr = np.mean(ar_Re_gr)
+print("Reynoldszahl große Kugel: ", num_Re_gr)
 
-print("Reynoldszahl dynamisch: ", Re(get_eta_gr(average_t)))
+#Reynoldazahl dynamisch
+
+# Reynoldszahl berechnen
+ar_Re = np.concatenate((np.array(Re_d(get_eta_gr(td_ou1))), np.array(Re_d(get_eta_gr(td_uo2))), np.array(Re_d(get_eta_gr(td_ou1))), np.array(Re_d(get_eta_gr(td_uo2)))))
+
+
+print("Reynoldszahl dynamisch: ", max(ar_Re), ar_Re, get_eta_gr(td_uo1[10]), td_uo1[10])
 
 # plt.show()
