31e8168ddfce190bb02c9d714a7aea4e8de17277
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-01-15 15:07:37 +0100

    Steigung umdefiniert und schönere Werte rausbekommen

--- v105/plot.py
+++ v105/plot.py
@@ -79,10 +79,13 @@ print("r offset: ", r_offset)
 
 # Daten
 fig1, ax1 = plt.subplots(layout="constrained")
-ax1.plot(get_B(I_Grav) / M_Masse * g, r_Grav, "x", label="Datenpunkte")
+#ax1.plot(get_B(I_Grav) / M_Masse * g, r_Grav, "x", label="Datenpunkte")
+ax1.plot(get_B(I_Grav), r_Grav, "x", label="Datenpunkte")
 
 # linregress
-params1, cov1 = np.polyfit(get_B(I_Grav) / M_Masse * g, r_Grav, deg=1, cov=True)
+#params1, cov1 = np.polyfit(get_B(I_Grav) / M_Masse * g, r_Grav, deg=1, cov=True)
+params1, cov1 = np.polyfit(get_B(I_Grav), r_Grav, deg=1, cov=True)
+
 err1 = np.sqrt(np.diag(cov1))
 x = np.linspace(20, 29)
 ax1.plot(x, x * params1[0] + params1[1], label=r"$\mu_{Dipol} \frac{B}{m g} + B$")
@@ -97,10 +100,12 @@ ax1.legend()
 fig1.savefig("build/Gravitation.pdf")
 
 # Ausgabe Ergebnis
-mu_Grav = ufloat(params1[0], err1[0])
+m_Grav = ufloat(params1[0], err1[0])
 B_Grav = ufloat(params1[1], err1[1])
-print("\nmu_Grav: ", mu_Grav)
+mu_Grav = M_Masse * g * m_Grav
+print("\nmu_Grav, also Steigung: ", mu_Grav)
 print("B_Grav: ", B_Grav)
+print("mg*steigung, also mu: ", M_Masse*g*mu_Grav)
 
 
 # Schwingung
@@ -108,26 +113,28 @@ print("B_Grav: ", B_Grav)
 
 # Daten
 fig2, ax2 = plt.subplots(layout="constrained")
-ax2.plot(
-    (T_Schwing**2) / (4 * J_K * np.pi**2),
-    1 / get_B(I_Schwing),
-    "x",
-    label="Datenpunkte",
-)
+#ax2.plot(
+#    (T_Schwing**2) / (4 * J_K * np.pi**2),
+#    1 / get_B(I_Schwing),
+#    "x",
+#    label="Datenpunkte",
+#)
+ax2.plot(1 / get_B(I_Schwing), T_Schwing**2, "x", label="Datenpunkte")
+
 
 # linregress
-params2, cov2 = np.polyfit(
-    (T_Schwing**2) / (4 * J_K * np.pi**2), 1 / get_B(I_Schwing), deg=1, cov=True
-)
+#params2, cov2 = np.polyfit( (T_Schwing**2) / (4 * J_K * np.pi**2), 1 / get_B(I_Schwing), deg=1, cov=True)
+params2, cov2 = np.polyfit(1 / get_B(I_Schwing), (T_Schwing**2), deg=1, cov=True)
+
 err2 = np.sqrt(np.diag(cov2))
 x = np.linspace(800, 3501)
 ax2.plot(
-    x, x * params2[0] + params2[1], label=r"$\mu_{Dipol}\frac{ T^2}{4 \pi ^2 J_K} + B$"
+    x, x * params2[0] + params2[1], label=r"$\frac{ T^2}{4 \pi ^2 J_K} + B$"
 )
 
 # settings
 ax2.set(
-    xlabel=r"$\frac{\mu_{Dipol} T^2}{4 \pi ^2 J_K}$",
+    xlabel=r"$\frac{T^2}{4 \pi ^2 J_K}$",
     ylabel=r"$\frac{1}{B}$",
     xlim=(800, 3501),
 )
@@ -135,10 +142,12 @@ ax2.legend()
 fig2.savefig("build/Schwingung.pdf")
 
 # Ausgabe Ergebnis
-mu_Schwing = ufloat(params2[0], err2[0])
+m_Schwing = ufloat(params2[0], err2[0])
 B_Schwing = ufloat(params2[1], err2[1])
+mu_Schwing = (J_K*4 * np.pi**2)/(m_Schwing)
 print("\nmu_Schwing: ", mu_Schwing)
 print("B_Schwing: ", B_Schwing)
+print("Steigung: ", m_Schwing)
 
 
 # Präzession
@@ -155,8 +164,16 @@ for i in range(np.size(T1_praz)):
 
 T_praz_total = unp.uarray(T_praz, T_praz_err)
 
-x = get_B(I_praz) / (2 * np.pi * J_K * 2 * np.pi / T_praz_total)
+#x = get_B(I_praz) / (2 * np.pi * J_K * 2 * np.pi / T_praz_total)
+x = get_B(I_praz)
+x2 = (get_B(I_praz) * T_praz_total) / (4 * np.pi **2 * J_K)
 y = 1 / T_praz_total
+
+#print("\nT_praz_total: ", T_praz_total)
+#print("\ny: ", y)
+#print("\nx: ", x)
+#print("\nx2: ", x2)
+#print(x==x2)
 
 fig3, ax3 = plt.subplots(layout="constrained")
 ax3.errorbar(
@@ -167,6 +184,7 @@ ax3.errorbar(
     fmt="x",
     label="Datenpunkte",
 )
+#ax3.plot(get_B(I_praz) / (2 * np.pi * J_K * 2 * np.pi / T_praz), 1 / T_praz, "x", label="Datenpunkte normal" )
 
 params3, cov3 = np.polyfit(unp.nominal_values(x),unp.nominal_values(y),deg=1,cov=True)
 err3 = np.sqrt(np.diag(cov3))
@@ -175,7 +193,7 @@ lin_x = np.linspace(12,17)
 ax3.plot(lin_x, lin_x * params3[0] + params3[1], label="Ausgleichsgerade")
 
 ax3.set(
-    xlabel=r"$\frac{\mu_{Dipol} B}{2 \pi L_K}$",
+    xlabel=r"$\frac{B}{2 \pi L_K}$",
     ylabel=r"$\frac{1}{T}$",
     xlim=(min(lin_x),max(lin_x))
 )
@@ -183,8 +201,10 @@ ax3.legend()
 fig3.savefig("build/Präzession.pdf")
 
 #Ausgabe Ergebnisse
-mu_Praz = ufloat(params3[0], err3[0])
+m_Praz= ufloat(params3[0], err3[0])
 B_Praz  = ufloat(params3[1], err3[1])
+mu_Praz = m_Praz*2 * np.pi * J_K * 2 * np.pi * 5.5
 
 print("\nmu_Praz: ", mu_Praz)
 print("B_Praz: ", B_Praz)
+print("m Steigung:", m_Praz)
