f3643856f88fa10070bd221782ad3fd52b16f3a3
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-04-09 10:52:55 +0200

    typos und basics in plot.py

--- v511/plot.py
+++ v511/plot.py
@@ -1,2 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+
+
+### Maaße und Grundgrößen ####
+breite = 0
+laenge = 0
+tiefe = 0
+e0 = 1.60217662e-19
+m0 = 9.10938356e-31
