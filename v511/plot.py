a0c5b529258af72fb75eb14e7eb48a44badf3fc7
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-04-11 19:01:08 +0200

    graphen fertig und erste auswertungen

--- v511/plot.py
+++ v511/plot.py
@@ -53,36 +53,100 @@ UH_constB_ent = -UH_constB_ent
 
 
 
-### Grundgrößen berechnen ###
+### Funktionen ###
+def Steigung(params, cov):
+    err = np.sqrt(np.diag(cov))
+    return ufloat(params[0], err[0])
+
+def Ladungstraegerdichte (m, d):                #Gleichung 6
+    n = - (1) / (e0 * m * d)
+    return n
+
+def mittlere_Flugzeit(L, n, Q, R):              #Gleichung 5
+    tau = (2 * m0 * L) / (e0**2 * n * Q * R)
+    return tau
 
 
 
 ### Daten plotten ###
-
+#
+#Orientierung an Gleichung 6
 #Kupfer
-fig_k, ax_k = plt.subplots(label="Kupfer")
-ax_k.plot(I_Kupfer, UH_Kupfer, "x", label="Kupfer")
+fig_k, ax_k = plt.subplots(label="Kupfer", layout = "constrained")
+ax_k.plot(I_Kupfer * B_Kupfer, UH_Kupfer, "x", label="Kupfer")
 ax_k.set(
-    xlabel="I/A",
-    ylabel=r"U_H / V"
+    xlabel="IB/AT",
+    ylabel=r"U_H / V",
+    xlim=[0,5]
 )
-params_k, cov = np.polyfit(I_Kupfer, UH_Kupfer, deg=1, cov=True)
-x = np.linspace(1,4)
-ax_k.plot(x, params_k[0]*x + params_k[1], label = "ax + b")
+params_k, cov_k = np.polyfit(I_Kupfer * B_Kupfer, UH_Kupfer, deg=1, cov=True)
+m_k = Steigung(params_k,cov_k)
+x = np.linspace(0,5)
+ax_k.plot(x, params_k[0]*x + params_k[1], label = f"ax + b \na = {m_k}")
 ax_k.legend()
 fig_k.savefig("./build/Kupfer.pdf")
 
+
 #Silber
 fig_s, ax_s = plt.subplots(label="Silber", layout = "constrained")
-ax_s.plot(I_Silber, UH_Silber, "x", label="Silber")
+ax_s.plot(I_Silber * B_Silber, UH_Silber, "x", label="Silber")
 ax_s.set(
-    xlabel="I/A",
-    ylabel=r"U_H / V"
+    xlabel="IB/AT",
+    ylabel=r"U_H / V",
+    xlim=[0,5]
 )
-params_s, cov = np.polyfit(I_Silber, UH_Silber, deg=1, cov=True)
-x = np.linspace(1,4)
-ax_s.plot(x, params_s[0]*x + params_s[1], label = f" ax + b \n a = {(params_s[0]*1e6):.3f} e-6")
+params_s, cov_s = np.polyfit(I_Silber * B_Silber, UH_Silber, deg=1, cov=True)
+m_s = Steigung(params_s,cov_s)
+x = np.linspace(0,5)
+ax_s.plot(x, params_s[0]*x + params_s[1], label = f" ax + b \n a = {m_s}")
 ax_s.legend()
 fig_s.savefig("./build/Silber.pdf")
 
+#Zink
+fig_z, ax_z = plt.subplots(label="Zink", layout = "constrained")
+ax_z.plot(I_Zink * B_Zink, UH_Zink, "x", label="Zink")
+ax_z.set(
+    xlabel="IB/AT",
+    ylabel=r"U_H / V",
+    xlim=[0,4.8]
+)
+params_z, cov_z = np.polyfit(I_Zink * B_Zink, UH_Zink, deg=1, cov=True)
+m_z = Steigung(params_z,cov_z)
+x = np.linspace(0,4.8)
+ax_z.plot(x, params_z[0]*x + params_z[1], label = f"ax + b \na = {m_z}")
+ax_z.legend()
+fig_z.savefig("./build/Zink.pdf")
 
+
+
+### Grundgrößen berechnen ###
+#
+#
+### Ladungsträgerdichte berechnen mit Gleichung 6 ###
+
+#Kupfer
+n_Kupfer = Ladungstraegerdichte(m_k, Kupfer_Folie_d)
+print("Die Ladungsträgerdichte n von Kupfer ist: ", n_Kupfer)
+
+#Silber
+n_Silber = Ladungstraegerdichte(m_s, Silber_Folie_d)
+print("Die Ladungsträgerdichte n von Silber ist: ", n_Silber)
+
+#Zink
+n_Zink = Ladungstraegerdichte(m_z, Zink_Folie_d)
+print("Die Ladungsträgerdichte n von Zink ist: ", n_Zink)
+
+
+### Mittlereflugzeit tau berechnen mit Gleichung 5 ###
+
+#Kupfer
+tau_Kupfer = mittlere_Flugzeit(Kupfer_Spule_l, n_Kupfer, np.pi * (0.5*Kupfer_Draht_d)**2, Kupfer_Spule_R)
+print("die mittlere Flugzeit eines elektrons im Kupfer ist: ", tau_Kupfer, "s")
+
+#Silber
+tau_Silber = mittlere_Flugzeit(Silber_Spule_l, n_Silber, np.pi * (0.5*Silber_Draht_d)**2, Silber_Spule_R)
+print("die mittlere Flugzeit eines elektrons im Silber ist: ", tau_Silber, "s")
+
+#Tantal
+tau_Tantal = mittlere_Flugzeit(Tantal_Spule_l, n_Tantal, np.pi * (0.5*Tantal_Draht_d)**2, Tantal_Spule_R)
+print("die mittlere Flugzeit eines elektrons im Tantal ist: ", tau_Tantal, "s")
