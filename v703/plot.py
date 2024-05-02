30f3466ce932513157d53eefd002a13b214be645
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-05-02 19:28:05 +0200

    Steigungen des Plateaus gemacht

--- v703/plot.py
+++ v703/plot.py
@@ -3,13 +3,19 @@ import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
 
-Kennlinie_U, val_Kennlinie_pulses, Strom_Kennlinie = np.genfromtxt("content/kennlinie.txt", unpack=True) # in V, 1, µA
+
+
+### Daten generieren
+Kennlinie_U, Kennlinie_pulses, Strom_Kennlinie = np.genfromtxt("content/kennlinie.txt", unpack=True) # in V, 1, µA
+
+
 
 #in SI umrechnen
 Strom_Kennlinie *= 1e-6 #in A
 
-#Spannung gegen Strom plotten
 
+
+#Spannung gegen Strom plotten
 fig, ax = plt.subplots()
 ax.errorbar(
     Kennlinie_U,
@@ -29,22 +35,19 @@ ax.legend()
 fig.savefig("build/Stromstaerke.pdf")
 
 
-### Fehler behaften
-Kennlinie_pulses = unp.uarray(val_Kennlinie_pulses, np.sqrt(val_Kennlinie_pulses))
 
 fig, ax = plt.subplots()
 ax.errorbar(
     Kennlinie_U,
-    unp.nominal_values(Kennlinie_pulses),
-    yerr=unp.std_devs(Kennlinie_pulses),
+    Kennlinie_pulses,
+    yerr=np.sqrt(Kennlinie_pulses),
     fmt="rx",
     capsize=2,
     label="Messdaten",
 )
 
 ### Ausgleichsgerade
-
-params_Kennlinie, cov_Kennlinie = np.polyfit(Kennlinie_U[4:19], val_Kennlinie_pulses[4:19], 1, cov=True, w= [1 / i for i in np.sqrt(val_Kennlinie_pulses)[4:19]])
+params_Kennlinie, cov_Kennlinie = np.polyfit(Kennlinie_U[4:19], Kennlinie_pulses[4:19], 1, cov=True, w= [1 / i for i in np.sqrt(Kennlinie_pulses)[4:19]])
 ax.plot(Kennlinie_U[4:19], params_Kennlinie[0] * Kennlinie_U[4:19] + params_Kennlinie[1], label="Ausgleichsgerade")
 
 
@@ -56,3 +59,18 @@ ax.set(
 )
 ax.legend()
 fig.savefig("build/Kennlinie.pdf")
+
+
+
+### Steigung des Plateus berechnen mit Formel
+
+print(
+    Kennlinie_U[6],
+    Kennlinie_U[9],
+    Kennlinie_U[12],    
+)
+s = (Kennlinie_pulses[12]- Kennlinie_pulses[6]) / Kennlinie_pulses[9]
+
+print("Die über die Formel berechnete Steigung des Plateaus ist: ", s)
+Steigung_Kennlinie = ufloat(params_Kennlinie[0], np.sqrt(np.diag(cov_Kennlinie))[0])
+print("Die über die Ausgleichsgerade berechnete Steigung des Plateaus ist: ", Steigung_Kennlinie)
