3fb389ea7a44c8c88ddf9226158a22078233942d
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-05-15 07:44:57 +0200

    Acrylblock Auswertung angefangen

--- US2/plot.py
+++ US2/plot.py
@@ -1,2 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from uncertainties import ufloat
+
+
+###Grundgrößen und Konstanten
+c_Acryl = 2730      #in m/s
+c_Wasser = 1497     #in m/s
+c_Linse = 2500      #in m/s
+c_Glaskörper = 1410 #in m/s
+H_Acrylblock = ufloat(0.079,0.000025)
+
+
+### Daten generieren
+H_top, H_bot = np.genfromtxt("./content/acrylblock.txt", unpack=True)         #in mm und mm
+t_top, t_bot = np.genfromtxt("./content/Laufzeit_Acryl.txt", unpack=True)     #in µs und µs
+Herz_T, Herz_Amp = np.genfromtxt("./content/Herz.txt", unpack=True)           #in  s und µs
+
+
+### Daten in SI umrechnen
+H_top *= 1e-3       #von mm in m
+H_bot *= 1e-3       #von mm in m
+
+t_top *= 1e-6       #von µs in s
+t_bot *= 1e-6       #von µs in s
+
+Herz_Amp *= 1e-6    #von µs in s
+
+
+
+### Durchmesser mithilfe der Gemessenen Höhen berechnen
+Durchmesser_Messschieber = H_Acrylblock - H_top - H_bot
+print("Die Durchmesser über den Messschieber bestimmt: ", Durchmesser_Messschieber)
+
+### Durchmesser mithilfe der Laufzeiten berechnen
+Durchmesser_Laufzeiten = H_Acrylblock - t_top * c_Acryl - t_bot * c_Acryl
+print("Die Durchmesser über die Laufzeiten bestimmt: ", Durchmesser_Laufzeiten)
+
+
+### Abweichungen bestimmen
+Durchmesser_Differenz = Durchmesser_Laufzeiten - Durchmesser_Messschieber
+print("Die Differenz zwischen den Methoden der Durchmesser ist: ", Durchmesser_Differenz)
+
+
+
