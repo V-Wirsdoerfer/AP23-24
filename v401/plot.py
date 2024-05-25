a5ffc13dced2bfe76b2cdbf19a1d79fab030e0e5
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-05-25 14:30:43 +0200

    Ausgabe verschönert

--- v401/plot.py
+++ v401/plot.py
@@ -5,17 +5,17 @@ from uncertainties import ufloat
 ### Daten generieren/Konstanten
 
 z_Wellenlaenge, z_EVK, z_BLF = np.genfromtxt("Zaehlung.txt", unpack=True)
-p0 = 1010e2,         #in pascal
+p0 = 1010e2,         # in pascal
 T0 = 23.7 + 273.15   # in Kelvin
 
 ### Wellenlänge berechnen
 
-x = ufloat(5e-3, 0.01e-3)
+x = ufloat(5e-3, 0.01e-3)   # in m
 
 z_Wellenlaenge_korr = z_Wellenlaenge[z_Wellenlaenge!=z_Wellenlaenge[2]]
 z_Wellenlange_mean = np.mean(z_Wellenlaenge_korr)
 Wellenlange = (2 * x) / (5.017 * z_Wellenlange_mean)
-print(Wellenlange)
+print("Die Wellenlänge beträgt: ", Wellenlange, " m")
 
 ### Berechnung des Brechungsindex
 
