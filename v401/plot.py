4025c671a3715508139d63f6a81c4b4ed552a01b
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-05-25 15:49:08 +0200

    Fehler zur Berechnung hinzugefügt

--- v401/plot.py
+++ v401/plot.py
@@ -5,15 +5,15 @@ from uncertainties import ufloat
 ### Daten generieren/Konstanten
 
 z_Wellenlaenge, z_EVK, z_BLF = np.genfromtxt("Zaehlung.txt", unpack=True)
-p0 = 1010e2,         # in pascal
-T0 = 23.7 + 273.15   # in Kelvin
+p0 = ufloat(1010e2, 0.01e-2)         # in pascal
+T0 = ufloat(23.7, 0.05) + 273.15   # in Kelvin
 
 ### Wellenlänge berechnen
 
 x = ufloat(5e-3, 0.01e-3)   # in m
 
 z_Wellenlaenge_korr = z_Wellenlaenge[z_Wellenlaenge!=z_Wellenlaenge[2]]
-z_Wellenlange_mean = np.mean(z_Wellenlaenge_korr)
+z_Wellenlange_mean = ufloat(np.mean(z_Wellenlaenge_korr), np.std(z_Wellenlaenge_korr))
 Wellenlange = (2 * x) / (5.017 * z_Wellenlange_mean)
 print("Die Wellenlänge beträgt: ", Wellenlange, " m")
 
