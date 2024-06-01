ebcb97a127d731a6a36b4771ad9fc6c088a81282
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-06-01 22:34:04 +0200

    einiges bla bla und so und oaar plots und korrekturen und hey kleinerer Fehler aber sieht nt mehr so toll aus meh ich geh fußball gucken

--- v407/plot.py
+++ v407/plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from uncertainties import unumpy as unp
 from uncertainties import ufloat
+from scipy.optimize import curve_fit
 
 
 #Daten generieren
@@ -42,10 +43,13 @@ fig1.savefig("build/Messdaten.pdf")
 
     
 #wurzel I/I0
+hilfs_Intensität_senkrecht = unp.uarray(Intensität_senkrecht, Fehler_senkrecht)
+hilfs_Intensität_parallel = unp.uarray(Intensität_parallel, Fehler_parallel)
+
 fig2, ax2 = plt.subplots(layout="constrained")
 
-ax2.errorbar(Winkel,  np.sqrt(Intensität_senkrecht/unp.nominal_values(I0)),xerr=0.5, yerr=np.sqrt(Fehler_senkrecht/unp.nominal_values(I0)) , fmt="x", label="senkrecht")
-ax2.errorbar(Winkel,  np.sqrt(Intensität_parallel/unp.nominal_values(I0)), xerr=0.5, yerr=np.sqrt(Fehler_parallel/unp.nominal_values(I0))  , fmt="x", label="parallel")
+ax2.errorbar(Winkel,  unp.nominal_values(unp.sqrt(hilfs_Intensität_senkrecht/I0)),xerr=0.5, yerr=unp.std_devs(unp.sqrt(hilfs_Intensität_senkrecht/I0)) , fmt="x", label="senkrecht")
+ax2.errorbar(Winkel,  unp.nominal_values(unp.sqrt(hilfs_Intensität_parallel/I0)), xerr=0.5, yerr=unp.std_devs(unp.sqrt(hilfs_Intensität_parallel/I0))  , fmt="x", label="parallel")
 ax2.set(
     xlabel="Winkel / °",
     ylabel=r"$\sqrt{\frac{I}{I_0}}$",
@@ -75,37 +79,129 @@ def Brechungsindex_senkrecht():
 
 def Brechungsindex_parallel(): 
     alpha = 2*np.pi * Winkel/360    #alpha in Radiant
-    n = 1/(2 * np.cos(alpha)**2 * (E_r_parallel - E_einfall)**2 ) + unp.sqrt(1 / (4 * np.cos(alpha)**4 * (E_r_parallel - E_einfall)**4) - np.tan(alpha)**2 * ( (E_r_parallel + E_einfall) / (E_r_parallel - E_einfall) )**2 )
+    n = (1)/(2 * np.cos(alpha)**2 * (E_r_parallel - E_einfall)**2 ) + unp.sqrt(1 / (4 * np.cos(alpha)**4 * (E_r_parallel - E_einfall)**4) - np.tan(alpha)**2 * ( (E_r_parallel + E_einfall) / (E_r_parallel - E_einfall) )**2 )
     return n 
 
-print("Brechungsindex: ", len(Brechungsindex_parallel()) )
-print("len Winkel: ", len(Winkel))
+print("Anzahl Brechungsindizes: ", len(Brechungsindex_parallel()) )
+print("Anzahl Winkel: ", len(Winkel))
+
+n_p = Brechungsindex_parallel()
+log_n_p = unp.log(n_p,10)
+log_log_n_p = unp.log(log_n_p)
+sqrt_log_n_p = unp.pow(log_n_p,1/2)
 
 
-### Brechungsindex einsehen
-fig3, ax3 = plt.subplots(2,2,layout="constrained")
-
-ax3 = np.ravel(ax3)
-ax3[0].errorbar(Winkel, unp.nominal_values(Brechungsindex_parallel()), yerr=unp.std_devs(Brechungsindex_parallel()), fmt="x", label="Brechungsindex \nparallel")
-ax3[1].errorbar(Winkel, np.log(unp.nominal_values(Brechungsindex_parallel())), yerr=np.log(unp.std_devs(Brechungsindex_parallel())), fmt="x", label="Brechungsindex \nparallel \nlogarithmiert")
-ax3[2].errorbar(Winkel, unp.nominal_values(Brechungsindex_senkrecht()), yerr=unp.std_devs(Brechungsindex_senkrecht()), fmt="x", label="Brechungsindex \nsenkrecht")
-ax3[3].errorbar(Winkel, np.log(unp.nominal_values(Brechungsindex_senkrecht())), yerr=abs(np.log(unp.std_devs(Brechungsindex_senkrecht()))), fmt="x", label="Brechungsindex \nsenkrecht \nlogarithmiert")
+n_s = Brechungsindex_senkrecht()
+log_n_s = unp.log(n_s)
+#log_log_n_s = unp.log(log_n_s)     #log_n_s wird negativ
+sqrt_n_s = unp.sqrt(n_s)
+sqrt_log_n_s = unp.pow(abs(log_n_s),1/2)
 
 
-ax3[0].set(
+### Brechungsindex einsehen parallel
+fig3, ax3 = plt.subplot_mosaic([["a","b"], ["c","d"]], layout="constrained")
+
+for loc, val, fmt in zip( ("acd"), (n_p, log_n_p, sqrt_log_n_p), ("xb", "xr", "xg") ):
+    ax3[loc].errorbar(Winkel, unp.nominal_values(val), yerr=unp.std_devs(val), fmt=fmt, label="Brechungsindex \nparallel")
+ax3["a"].set(
     xlabel = "Winkel/°",
-    ylabel = "n",
+    ylabel = r"$n_\parallel$",
     yscale = "log"
 )
-
-ax3[2].set(
+ax3["c"].set(
     xlabel = "Winkel/°",
-    ylabel = "n",
-    yscale = "log"
+    ylabel = r"$\ln{(n_\parallel)}$",
+)
+ax3["d"].set(
+    xlabel = "Winkel/°",
+    ylabel = r"$\sqrt{-\ln{(n_\parallel)}}$i",
 )
 
-for i in range(len(ax3)):
-    print(i)
-    ax3[i].legend(loc = "upper left")
+ax3["b"].set_axis_off()
+for label, fmt in zip(("\nBrechungsindex parallel \nunverändert","\nBrechungsindex parallel \nlogarithmiert ","\nBrechungsindex parallel \nlogarithmiert \nwurzel gezogen"), ("brg")):
+    ax3["b"].plot(1,1, color=fmt, label=label)
+ax3["b"].legend()
 
-fig3.savefig("build/Brechungsindex.pdf")
+fig3.savefig("build/Brechungsindex_parallel.pdf")
+
+
+### Brechungsindex orthogonal/senkrecht polarisiert
+fig4, ax4 = plt.subplot_mosaic([["left", "legend"], ["lower left", "lower right"]], layout="constrained")
+
+ax4["left"].errorbar(Winkel, unp.nominal_values(n_s), yerr=unp.std_devs(n_s), fmt="bx", label="Brechungsindex \nsenkrecht")
+ax4["lower left"].errorbar(Winkel, unp.nominal_values(log_n_s), yerr=unp.std_devs(log_n_s), fmt="rx", label="Brechungsindex \nsenkrecht logarithmiert")
+ax4["lower right"].errorbar(Winkel, unp.nominal_values(sqrt_log_n_s), yerr=abs(unp.std_devs(sqrt_log_n_s)), fmt="gx", label="Brechungsindex \nsenkrecht \nlogarithmiert gewurzelt")
+
+ax4["legend"].plot(1,1,color ="b",label="\nBrechungsindex senkrecht")
+ax4["legend"].plot(1,1,color ="r",label="\nBrechungsindex senkrecht \nlogarithmiert")
+ax4["legend"].plot(1,1,color ="g",label="\nBrechungsindex senkrecht \nlogarithmiert \nwurzel gezogen")
+
+ax4["legend"].set_axis_off()
+ax4["legend"].legend()
+
+ax4["left"].set(
+    xlabel = "Winkel/°",
+    ylabel = r"$n_\bot$",
+    yscale = "log"
+)
+ax4["lower left"].set(
+    xlabel = "Winkel/°",
+    ylabel = r"$\ln{(n_\bot)}$",
+)
+ax4["lower right"].set(
+    xlabel = "Winkel/°",
+    ylabel = r"$\sqrt{-\ln{(n_\bot)}}$i",
+)
+
+fig4.savefig("build/Brechungsindex_orthogonal.pdf")
+
+
+
+### Fit von Gleichung 18 und 21 aus Anleitung an Graphen
+
+def E_r_orth(alpha, n, a):     #Gleichung 18 / sqrt(I0) also /E_einfall
+    '''
+    Gleichung 18
+    Gibt die Amplitude der Reflektierten Strahlung zurück
+    nimmt nur Werte ohne Fehler
+    Gibt Werte mit Fehler zurück
+    '''
+    alpha = 2*np.pi * alpha / 360       #alpha in Radiant umrechnen
+    E =  (np.sqrt(n**2 - (np.sin(alpha))**2 ) - np.cos(alpha) )**2 / (n**2 -1) + a
+    return E
+
+def E_r_parallel(alpha, n, a): #Gleichung 21 / sqrt(I0) also /E_einfall
+    '''
+    Gleichung 21
+    Gibt die Amplitude der Reflektierten Strahlung zurück
+    nimmt nur Werte ohne Fehler
+    Gibt Werte mit Fehler zurück
+    '''
+    alpha = 2*np.pi * alpha / 360       #alpha in Radiant umrechnen
+    E = (n**2 * np.cos(alpha) - np.sqrt(n**2 - (np.sin(alpha))**2) ) / ( n**2 * np.cos(alpha) + np.sqrt(n**2 - (np.sin(alpha))**2) ) + a
+    return E
+
+params_senkrecht, cov_senkrecht = curve_fit(E_r_orth, Winkel, unp.nominal_values(unp.sqrt(hilfs_Intensität_senkrecht/I0)), p0=[7,-0.37])
+
+x_alpha = np.linspace(0, 90,10000)
+ax2.plot(x_alpha, E_r_orth(x_alpha, 7, -0.37), label="geratener Plot")
+ax2.plot(x_alpha, E_r_orth(x_alpha, *params_senkrecht), label ="fit")
+
+
+params_parallel, cov_parallel = curve_fit(E_r_parallel, Winkel, unp.nominal_values(unp.sqrt(hilfs_Intensität_parallel/I0)), p0=[7, -0.5])
+params_parallel2, cov_parallel2 = curve_fit(E_r_parallel, Winkel[Winkel <=74], unp.nominal_values(unp.sqrt(hilfs_Intensität_parallel/I0)[Winkel<=74]), p0=[7, -0.5])
+
+x_alpha = np.linspace(0, 90,10000)
+x_alpha2 = np.linspace(0, 74,10000)
+ax2.plot(x_alpha, E_r_parallel(x_alpha, 12, -0.54), label="geratener Plot")
+ax2.plot(x_alpha, E_r_parallel(x_alpha, *params_parallel), label ="fit")
+ax2.plot(x_alpha2, E_r_parallel(x_alpha2, *params_parallel2), label ="fit")
+
+
+ax2.legend()
+ax2.set(ylim=[0,0.7])
+fig2.savefig("build/I0_fit.pdf")
+
+print("senkrecht, n, a, b:", *params_senkrecht)
+print("parallel, n, a, b:", *params_parallel)
+
