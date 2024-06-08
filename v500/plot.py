c1a547268b8b65e543450b2de32a50407c6bfa03
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-06-08 11:21:58 +0200

    Daten erstellt

--- v500/plot.py
+++ v500/plot.py
@@ -1,2 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+### Daten generieren 
+U_Blau_fein, I_Blau_fein = np.genfromtxt("blau_fein.txt", unpack=True)
+U_Blau_grob, U_Blau_grob = np.genfromtxt("blau_grob.txt", unpack=True)
+
