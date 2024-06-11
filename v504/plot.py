090960147c5023e8910229cc8c14016789625540
Vincent Wirsdoerfer <vincent.wirsdoerfer@tu-dortmund.de>
2024-06-11 14:36:42 +0200

    txt Dateien erstellt

--- v504/plot.py
+++ v504/plot.py
@@ -3,11 +3,11 @@ import numpy as np
 
 ### Daten generieren
 
-I_Anode, U = np.genfromtxt("Strom_Spannung.txt")
+I_Anode, U = np.genfromtxt("Kennlinie2.4.txt", unpack=True)
 
 
 
 ###Testplot
 fig1, ax1 = plt.subplots(layout="constrained")
-ax1.plot(I_Anode,U, "x", label="Testplot")
+ax1.plot(I_Anode, U, "x", label="Testplot")
 plt.show()
