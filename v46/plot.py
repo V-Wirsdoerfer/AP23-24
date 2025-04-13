0b2bf3b720e7805f16c5ef1138b72869d0fe9d06
Vincent Wirsdörfer <vincent.wirsdoerfer@udo.edu>
2025-04-13 22:26:27 +0200

    v46 erstellt

---
+++ v46/plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+
+c = const.c
+e = const.e
+pi= const.pi
+epsilon_0 = const.epsilon_0
+
+print(c,e,pi,epsilon_0)
