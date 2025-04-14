cf44834bce923b916fead5298bc917712ddd5950
Vincent Wirsdörfer <vincent.wirsdoerfer@udo.edu>
2025-04-14 15:01:39 +0200

    plot.py angefangen

--- v46/plot.py
+++ v46/plot.py
@@ -7,4 +7,30 @@ e = const.e
 pi= const.pi
 epsilon_0 = const.epsilon_0
 
-print(c,e,pi,epsilon_0)
+#Daten generieren
+x, B = np.genfromtxt("content/Magnetfeld.txt", unpack=True)
+
+
+
+
+#Daten plotten
+fig1, ax1 = plt.subplots(layout="constrained")
+ax1.plot(x,B, label="Magnetfeldstärke")
+ax1.legend()
+ax1.set(
+    xlabel="Abstand zur Mitte in mm",
+    ylabel="Magnetfeldstärke in mT"
+)
+fig1.savefig("Magnetfeld.pdf")
+
+
+fig2, ax2 = plt.subplots(layout="constrained")
+ax2.plot(x,B, label="Winkel, 1_2e18")
+ax2.legend()
+ax2.set(
+    xlabel="Wellenlänge des Lichts in µm",
+    ylabel="Winkeländerung"
+)
+fig2.savefig("1_2e18.pdf")
+
+
